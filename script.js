f7b51fb984bb23a9a8f45f57c449b7a646f057a2
SkyWingDesign <sreeshanthubr999@gmail.com>
2023-09-10 19:10:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+   
+    
+    
+  
+    let header = document.querySelector('.header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+};
+
+
+var swiper = new Swiper(".mySwiper",{
+    slidePerView:1,
+    spaceBetween:50,
+    loop: true ,
+    grabCursor: true,
+    pagination:{
+        el:".swiper-pagination",
+        clickeble : true,
+    },
+  
+});
